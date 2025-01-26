d45bb3298a6876c0a8596c614efc5a787a1d0c8a
MmDZ21 <mmohamadzz294@gmail.comgit config --global user.name MmDZ21git config --global user.email mmohamadzz294@gmail.com>
2025-01-26 11:47:39 +0330

    change dashboard to dynamic render

---
+++ app/(root)/dashboard/layout.tsx
@@ -0,0 +1,27 @@
+import Footer from "@/components/layout/footer";
+import Header from "@/components/layout/header";
+import Image from "next/image";
+import React from "react";
+
+export default async function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="min-h-screen w-full flex flex-col items-center">
+      {/* Dashboard Header Image */}
+      <div className="w-full h-[293px] lg:h-[500px] relative">
+        <Image
+          priority
+          src="/images/dashboard.webp"
+          fill
+          alt="anime"
+          className="object-cover object-top"
+        />
+        <div className="absolute h-1/2 lg:h-full bottom-0 left-0 right-0 z-10 dark:bg-gradient-to-t dark:from-background dark:to-background/0"></div>
+      </div>
+      {children}
+    </div>
+  );
+}
--- app/(root)/dashboard/page.tsx
+++ app/(root)/dashboard/page.tsx
@@ -15,19 +15,7 @@ import { cn } from "@/lib/utils";
 export const dynamic = "force-dynamic";
 export default async function Page() {
   return (
-    <div className="min-h-screen w-full flex flex-col items-center">
-      {/* Dashboard Header Image */}
-      <div className="w-full h-[293px] lg:h-[500px] relative">
-        <Image
-          priority
-          src="/images/dashboard.webp"
-          fill
-          alt="anime"
-          className="object-cover object-top"
-        />
-        <div className="absolute h-1/2 lg:h-full bottom-0 left-0 right-0 z-10 dark:bg-gradient-to-t dark:from-background dark:to-background/0"></div>
-      </div>
-
+    <>
       {/* Main Dashboard Content */}
       <div className="w-full flex flex-col items-center gap-4 lg:gap-6 max-w-[1280px] pb-4 -mt-12 lg:-mt-44 z-20 lg:px-4">
         {/* User Info */}
@@ -93,6 +81,6 @@ export default async function Page() {
           ))}
         </Tabs>
       </div>
-    </div>
+    </>
   );
 }
