f5ac33aec647e15d7bfc468be80f9dae045eec8d
MmdZ21 <mmohammadzz294@gmail.com>
2025-03-18 19:15:57 +0330

    membership function fix

--- actions/payReq.ts
+++ actions/payReq.ts
@@ -4,21 +4,21 @@ import { getAuthClient } from "@/lib/apolloClient";
 import { redirect } from "next/navigation";
 
 export default async function payReq(formData: FormData) {
-  console.log(JSON.stringify(formData))
+  console.log(JSON.stringify(formData));
   const client = getAuthClient();
-  const { data, errors} = await client.mutate<PayReqMutation>({
+  const { data, errors } = await client.mutate<PayReqMutation>({
     mutation: PayReqDocument,
-    variables:{
-        input:{
-          amount:Number(formData.get("amount")),
-          callback_url:"http://localhost:3000/dashboard/payment"
-        }
-    }
+    variables: {
+      input: {
+        amount: Number(formData.get("amount")),
+        callback_url: process.env.WEBSITE_URL + "dashboard/payment",
+      },
+    },
   });
   if (errors) {
     console.error("Error fetching dashboard data:", errors);
     throw new Error("Error fetching dashboard data");
   }
 
-  redirect(data?.createPaymentRequest.redirect_url || "/404")
+  redirect(data?.createPaymentRequest.redirect_url || "/404");
 }
