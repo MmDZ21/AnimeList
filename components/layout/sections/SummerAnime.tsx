5f6766ef9e9376ed50be493b46b4b78fa4c1077a
MmdZ21 <mmohammadzz294@gmail.com>
2025-03-23 16:29:06 +0330

    slider

--- components/layout/sections/SummerAnime.tsx
+++ components/layout/sections/SummerAnime.tsx
@@ -1,13 +1,13 @@
-import React from "react";
+// import React from "react";
 
-import dynamic from "next/dynamic";
-const MediaCarousels = dynamic(() => import("../carousels/MediaCarousels"));
-import SectionContainer from "./SectionContainer";
+// import dynamic from "next/dynamic";
+// const MediaCarousels = dynamic(() => import("../carousels/MediaCarousels"));
+// import SectionContainer from "./SectionContainer";
 
-export default function SummerAnime() {
-  return (
-    <SectionContainer title="انیمه‌های فصل تابستان">
-      <MediaCarousels />
-    </SectionContainer>
-  );
-}
+// export default function SummerAnime() {
+//   return (
+//     <SectionContainer title="انیمه‌های فصل تابستان">
+//       <MediaCarousels />
+//     </SectionContainer>
+//   );
+// }
--- components/layout/sections/trending/Parallel.tsx
+++ components/layout/sections/trending/Parallel.tsx
@@ -1,28 +1,28 @@
-"use client"
-import React from "react";
-import Parallel from "../Parallel";
-import MediaCarousels from "../../carousels/MediaCarousels";
-import { recommendations } from "@/constants";
-import SeeAllButton from "./SeeAllButton";
-import { useSuspenseQuery } from "@apollo/client";
-import { GetSeasonalAnimesDocument, GetSeasonalAnimesQuery } from "@/generated/graphql";
+// "use client"
+// import React from "react";
+// import Parallel from "../Parallel";
+// import MediaCarousels from "../../carousels/MediaCarousels";
+// import { recommendations } from "@/constants";
+// import SeeAllButton from "./SeeAllButton";
+// import { useSuspenseQuery } from "@apollo/client";
+// import { GetSeasonalAnimesDocument, GetSeasonalAnimesQuery } from "@/generated/graphql";
 
-export default function TrendingParallel() {
-  const { data } = useSuspenseQuery<GetSeasonalAnimesQuery>(GetSeasonalAnimesDocument, {
-    variables: {
-      first: 6,
-    },
-  });
-  const animes= data?.animesSeason.data || []
-  return (
-    <Parallel
-      bgGradient="to top, hsla(15,76%,13%,0.63), hsla(25,95%,53%,0.7)"
-      bgUrl="/images/trending.webp"
-      title="محبوب‌ترین‌های فصل"
-      description="آثار محبوب این فصل که نباید از دست‌شون بدی"
-      actions={[<SeeAllButton key={1} />]}
-    >
-      <MediaCarousels />
-    </Parallel>
-  );
-}
+// export default function TrendingParallel() {
+//   const { data } = useSuspenseQuery<GetSeasonalAnimesQuery>(GetSeasonalAnimesDocument, {
+//     variables: {
+//       first: 6,
+//     },
+//   });
+//   const animes= data?.animesSeason.data || []
+//   return (
+//     <Parallel
+//       bgGradient="to top, hsla(15,76%,13%,0.63), hsla(25,95%,53%,0.7)"
+//       bgUrl="/images/trending.webp"
+//       title="محبوب‌ترین‌های فصل"
+//       description="آثار محبوب این فصل که نباید از دست‌شون بدی"
+//       actions={[<SeeAllButton key={1} />]}
+//     >
+//       <MediaCarousels />
+//     </Parallel>
+//   );
+// }
